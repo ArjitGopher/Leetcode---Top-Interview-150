602dafefa8f7cc6ca80de7ef9b6f076126ef549c
arjit agarwal <agarwalarjit.agarwal@gmail.com>
2025-09-27 23:37:36 +0530

    feat: add remove_element

---
+++ remove_element/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+      int i=-1;
+      for(int j=0;j<nums.size();j++){
+        if(nums[j]!=val){
+            swap(nums[j],nums[++i]);
+        }
+      }
+      return i+1;  
+    }
+};
